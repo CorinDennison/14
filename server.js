b726b5c06ab89c3daf31f11b2d014a15fec3fc2f
CorinDennison <corin.dennison@gmail.com>
2025-05-08 20:54:35 -0500

    commit after deploy that seemed to break it

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const apiRoutes = require("./routes/api.js")
+const path = require('path')
 
 // Crete web app server
 const app = express()
 
 app.use(express.json())
+
+const staticFilePath = path.join(__dirname, 'client', 'dist')
+const staticFiles = express.static(staticFilePath)
+app.use('/', staticFiles)
+
 app.use('/api', apiRoutes)
 
 app.use(function(req, res, next) {
